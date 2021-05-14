9760897e849f3be4458a5094de277e7870ad4792
nikolayonok <nikolayonokk@yandex.ru>
2021-05-14 21:16:05 +0300

    task5

---
+++ src/task5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the time:");
+        int time = sc.nextInt();
+        if (time > 0 && time <= 6) {
+            System.out.println("Good night");
+        }
+        if (time > 6 && time <= 13) {
+            System.out.println("Good morning");
+        }
+        if (time > 13 && time <= 17) {
+            System.out.println("Good day");
+        }
+        if (time > 17 && time <= 24) {
+            System.out.println("Good evening");
+        } else {
+            System.err.println("Not a time");
+        }
+
+    }
+}
